b2311297fb000e6a72598d47d7be39bc62ca9042
ruggerygusmao <ruggery402@gmail.com>
2021-07-13 14:38:50 -0300

    inicio do alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,71 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(propriedades){
+  return(
+      <Box>
+        <img src={`https://github.com/${propriedades.githubUser}.png`} style={{borderRadius: '8px'}}></img>
+      </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'ruggerygusmao';
+  const pessoasFavoritas = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho',
+  ]
+
+  return (
+   <>
+   <AlurakutMenu />
+  <MainGrid>
+    <div className="profileArea" style={{gridArea: 'profileArea'}}>
+      <ProfileSidebar githubUser={githubUser}/>
+    </div>
+    <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+      <Box>
+       <h1 className= "title"> 
+          Bem vindo(a) 
+       </h1>
+
+       <OrkutNostalgicIconSet />
+      </Box>
+
+      
+    </div>
+    <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+      <Box>
+        <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle">
+        Pessoas da Comunidade
+        </h2>
+        <ul>
+        {pessoasFavoritas.map((itemAtual) => {  
+        return (
+          <li>
+            <a href={`/users/${itemAtual}`} key={itemAtual}>
+              <img src={`https://github.com/${itemAtual}.png`} />
+              <span>{itemAtual}</span>
+            </a>
+            </li>
+        )
+          })}
+           </ul>
+           </ProfileRelationsBoxWrapper>
+      </Box>
+     
+      <Box>
+        Comunidades
+      </Box>
+    </div>
+  </MainGrid>
+  </>
+  )
 }
